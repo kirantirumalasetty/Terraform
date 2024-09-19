d23f8c43b17e1437a1216a208f8adc3db3a80605
kiran <kirantwls@gmail.com>
2024-09-19 18:20:27 +0530

    terraform practice

---
+++ Terraform/remote-state/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.66.0"
+        }
+    }
+    backend "s3" {
+        bucket = "krn-remote-state"
+        key = "remote-state-demo"
+        region = "us-east-1"
+        dynamodb_table = "state-locking"
+    }
+}
