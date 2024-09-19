0c215a25a90d99eafe7574f61c7f53a4f165fe2f
kiran <kirantwls@gmail.com>
2024-09-19 20:21:47 +0530

    terraform practice

---
+++ Terraform/for_each/ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "terraform" {
+    for_each = var.instances
+    ami = "ami-0182f373e66f89c85"
+    instance_type = each.value
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = {
+        Name = each.key
+    }
+}
+
+resource "aws_security_group" "allow_ssh_terraform" {
+    name = "allow-ssh"
+    description = "allow port number 22 for SSH access"
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    tags = {
+        Name = "allow_ssh"
+    }
+}
---
+++ Terraform/for_each/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.66.0"
+        }
+    }
+    backend "s3" {
+        bucket = "krn-remote-state"
+        key = "for-each-demo"
+        region = "us-east-1"
+        dynamodb_table = "state-locking"
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
